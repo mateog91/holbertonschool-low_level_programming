c1d242b5e5568e9ee4e9adfaba1099391d6c305c
Mateo <mateog91@gmail.com>
2021-06-18 21:29:39 +0000

    added file 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Print lowercase alphabet followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	int z;
+	int i;
+
+	/* your code goes there */
+	/* Variables Initialization */
+	a = 97;
+	z = 122;
+	i = a;
+	/*Go from a to z (lower case) in the ASCII table and print each character*/
+	while (i <= z)
+	{
+		putchar(i);
+		i++;
+	}
+	/*Print new line after the alphabet*/
+	putchar('\n');
+	return (0);
+}
