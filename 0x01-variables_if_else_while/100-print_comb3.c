56011003655338e9992bb6a50ce7ad42516f7b59
Mateo <mateog91@gmail.com>
2021-06-19 00:57:30 +0000

    added bigger while with counter j, testing where should the restart should go, inside or outside the inner while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -19,20 +19,29 @@ int main(void)
 	first = 48;
 	last = 57;
 	i = first;
+	j = i;
 	/*
 	 * Goes from first to last printing the current value
 	 * and restarts +1 when reaching last number
 	 */
-	while (i <= last)
+	while (j <= last)
 	{
-		putchar(i);
-		i++;
-		/* Restart variables */
-		if (i > last)
-		{
-			first++;
-			i = first;
+		while (i <= last)
+		{	
+			putchar(j);
+			putchar(i);
+			printf (", "); /*MUST BE REMOVED*/
+			i++;
+			/* Restart variables +1 */
+			if (i > last)
+			{
+				first++;
+				i = first;
+				printf ("\n");/*MUST BE REMOVED*/
+			}
 		}
+		/* Add +1 to counter j*/
+		j++;
 	}
 	/* Print new line */
 	putchar('\n');
