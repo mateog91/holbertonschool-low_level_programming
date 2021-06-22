cd4150ecb51741ecc431c2ad0daef7a74d86524f
Mateo <mateog91@gmail.com>
2021-06-22 23:42:30 +0000

    correctly modified function documentation comments

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,25 +1,29 @@
 #include <unistd.h>
 #include "holberton.h"
 /**
- * print_alphabetx10 - prints lowercase alphabet 10 times seperated by new line
+ * print_alphabet - prints lowercase alphabet ending with a new line
  *
- * Return: void
  */
 void print_alphabet(void)
 {
-        int i = 97, z = 122;
+	int i = 97, z = 122;
 
-        while (i <= z)
-        {
-                _putchar(i);
-                i++;
-        }
-        _putchar('\n');
+	while (i <= z)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
 }
 
+/**
+ * print_alphabet_x10 - prints lowercase alphabet 10 times seperated by newline
+ *
+ */
 void print_alphabet_x10(void)
 {
 	int i = 1;
+
 	while (i <= 10)
 	{
 		print_alphabet();
