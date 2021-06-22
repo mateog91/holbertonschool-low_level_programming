655b52e40b45aa11721a9bc132dc5d8048c73e93
Mateo <mateog91@gmail.com>
2021-06-22 15:47:56 +0000

    added correct files .c and .h

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * main - Prints Holberton without using libraries, using built in functions
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
