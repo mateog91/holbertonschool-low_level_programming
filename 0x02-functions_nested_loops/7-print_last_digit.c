fef06ac6641146a8441cdee6189594785b51c17e
Mateo <mateog91@gmail.com>
2021-06-23 03:57:11 +0000

    added if for negative numbers and used only variable n

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,8 +8,9 @@
  */
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
-
-	_putchar('0' + last_digit);
-	return (last_digit);
+	if (n < 0)
+		n = n * (-1);
+	n = n % 10;
+	_putchar('0' + n);
+	return (n);
 }
