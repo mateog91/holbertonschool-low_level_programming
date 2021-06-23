e78acdadcaa2e49860933b11d5b916e5e6202ef4
Mateo <mateog91@gmail.com>
2021-06-23 22:36:40 +0000

    did it again correctly, changed everything

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,5 +5,41 @@
  */
 void times_table(void)
 {
-	_putchar('0');
+	int fila = 0, columna, resultado;
+
+	while (fila <= 9)
+	{
+		columna = 0;
+		while (columna <= 9)
+		{
+			resultado = fila * columna;
+			if (columna == 0)
+			{
+				_putchar('0' + resultado);
+			}
+
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (resultado / 10 == 0)
+				{
+					/**
+					 * Imprime espacio adicional para
+					 * numeros de mas de un digito
+					 */
+					_putchar(' ');
+					_putchar('0' + resultado);
+				}
+				else
+				{
+					_putchar('0' + resultado / 10);
+					_putchar('0' + resultado % 10);
+				}
+			}
+			columna++;
+		}
+		fila++;
+		_putchar('\n');
+	}
 }
