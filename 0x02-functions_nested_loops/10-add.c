94c1ba01361d9e0cfeee2c721ead681b74bc8557
Mateo <mateog91@gmail.com>
2021-06-23 04:34:55 +0000

    added file 10 and updated prototype on holberton.h

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * int add(int a, int b); - adds two intergers
+ * @a: Fist interger
+ * @b: Second interger
+ *
+ * Return: Sum of two intergers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
