20d7ee6c887127ef96848fdece04064e8584a703
Mateo <mateog91@gmail.com>
2021-06-23 04:21:21 +0000

    added file 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - prints every minute of day
+ */
+void jack_bauer(void);
+{
+	int hh = 0, mm = 0;
+
+	while (hh < 24)
+	{
+		while (mm < 60)
+		{
+			_putchar('0' + (hh / 10));
+			_putchar('0' + (hh % 10));
+			_putchar(':');
+			_putchar('0' + (mm / 10));
+			_putchar('0' + (mm % 10));
+			mm++;
+		}
+		hh++;
+		mm = 0;
+	}
+}
