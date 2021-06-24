dc07a5fcab38d9bb80c276d5614df26b86dd898a
Mateo <mateog91@gmail.com>
2021-06-24 15:55:40 +0000

    added file 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+/**
+ * _isupper - checks for uppercase character
+ *
+ * Return: 1 if uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	int first = 65, last = 90;
+
+	if (c >= first && c <= last)
+		return (1);
+	else
+		return (0)
+}
