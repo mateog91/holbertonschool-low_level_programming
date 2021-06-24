405d68f4943c15420eee28470fa7ac35961eaa9c
Mateo <mateog91@gmail.com>
2021-06-24 18:04:59 +0000

    changed to while loop

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,19 @@
 #include "holberton.h"
 
 /**
- * print_number - prints numbers 0 through 9
+ * print_numbers - prints numbers 0 through 9
  *
  * Return: Always 0.
  */
 
 void print_numbers(void)
 {
-	int first = 48, last = 57, current;
+	int first = 48, last = 57, current = first;
 
-	for (current = first; current <= last; current++)
+	while (current <= last)
 	{
 		_putchar(current);
+		current++;
 	}
 	_putchar('\n');
-	return();
 }
