2df16e58d9a9b13c3b7e9384043c1af632fd2e81
Mateo <mateog91@gmail.com>
2021-06-29 20:23:06 +0000

    added file 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _puts2 - prints every other char of string followed by a new line
+ * @str: - pointer to first character of string
+ *
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		if (i % 2 == 0)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
