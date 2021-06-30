6cba851192d4d345f33927e2f4b45cce6c290052
Mateo <mateog91@gmail.com>
2021-06-30 00:15:05 +0000

    added file 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_rev - prints a string in reverse followed by new line
+ * @s: input string
+ *
+ */
+void rev_string(char *s)
+{
+
+	int i, length;
+	char c;
+
+	/* length of s */
+	for (length = 0; s[length]; length++)
+	{
+	}	
+
+	length--;
+	for (i = 0; i <= (length / 2); i++)
+	{
+		c = s[i];
+		s[i] = s[length - i];
+		s[length - i] = c;
+	}
+}
