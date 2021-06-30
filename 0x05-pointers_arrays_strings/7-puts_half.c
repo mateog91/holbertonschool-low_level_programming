9370cf0c51380c223e27a2fa3777f0ba8984264f
Mateo <mateog91@gmail.com>
2021-06-30 01:58:11 +0000

    removed len-- statemente

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
  * puts_half - prints the second half of a string followed by a new line
  * @str: - pointer to first character of string
@@ -12,15 +12,22 @@ void puts_half(char *str)
 
 	/* finds length of string */
 	for (len = 0; str[len]; len++)
-
+	{
+	}
+	printf("valor de len: %d\n", len);
+	/* len--; */
 	/* finds start of second half */
-	if ((len + 1) % 2 != 0)
-		n = (len - 1) / 2;
+	if ((len) % 2 != 0)
+		n = (len - 0) / 2;
 	else
-		n = len / 2 + 1;
+		n = len / 2;
 
+	printf("valor de n: %d\n", n);
 	/* prints string from n to the end */
 	for (; str[n]; n++)
+	{
+	/*	printf("valor actual de n dentro del for es: %d\n", n); */
 		_putchar(str[n]);
+	}
 	_putchar('\n');
 }
