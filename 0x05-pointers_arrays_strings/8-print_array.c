7c1e991fa1d9520439df83b77881b1b1d9ccb984
Mateo <mateog91@gmail.com>
2021-06-30 02:31:44 +0000

    documentation corrected and added else in if

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,9 +2,9 @@
 #include "holberton.h"
 
 /**
- print_array - prints elements of an array of intergers followed by new line
- @a: array of intergers
- @n: number of elements in array
+ * print_array - prints elements of an array of intergers followed by new line
+ * @a: array of intergers
+ * @n: number of elements in array
  *
  */
 void print_array(int *a, int n)
@@ -16,7 +16,8 @@ void print_array(int *a, int n)
 		if (i == 0)
 			printf("%d", *a);
 
-		printf(", %d", *(a + i));
+		else
+			printf(", %d", *(a + i));
 	}
 	printf("\n");
 }
