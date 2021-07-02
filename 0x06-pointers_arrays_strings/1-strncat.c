6819c89cfc1d86a05833a6e86eed204b53cef3e3
Mateo <mateog91@gmail.com>
2021-07-02 17:25:22 +0000

    added new folder and files 0 and 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,69 @@
+#include "holberton.h"
+
+int length(char *string);
+
+/**
+ * _strncat - appends the src string to dest
+ * @dest: address to string desteny
+ * @src: address to string source
+ * @n: input interger of n chars to copy from src to dest
+ *
+ * Descritption:
+ * Appends the first n chars of src string to the dest string, overwriting the
+ * null byte (\0) at the end of dest, and then adds a terminating null byte.
+ *
+ * Return:A pointer to the resulting string dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	/* Variable Declaration */
+	int i = 0;
+	int len_dest;
+
+	/* find len of dest */
+	len_dest = length(dest);
+
+	/* pass chars of src to dest  */
+	while (1)
+	{
+		/* finished passing n, append null and break */
+		if (i > n)
+		{
+			dest[len_dest + i] = '\0';
+			break;
+		}
+
+		/* pass char of src to dest*/
+		dest[len_dest + i] = src[i];
+
+		/* find null char of src and stop*/
+		if (src[i] == '\0')
+		{
+			break;
+		}
+		/* increase i */
+		i++;
+	}
+
+	return (dest);
+
+}
+/**
+ * length - finds length of a string
+ * @string: Input string
+ *
+ * Description:
+ * Goes through chars of input strings, counts
+ * and returns an interger with its length.
+ *
+ * Return: Interger with length of string
+ */
+int length(char *string)
+{
+	int i;
+
+	for (i = 0; string[i]; i++)
+	{
+	}
+	return (i);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Exersises for 0x06
