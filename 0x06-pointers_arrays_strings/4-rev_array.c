3dc79f9692d65df07099e52392f00f12fbdf23be
Mateo <mateog91@gmail.com>
2021-07-02 19:18:37 +0000

    added documentation to function

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,12 +1,15 @@
 /**
+ * reverse_array - reverses the content of an array of integers.
+ * @a: pointer to first element of array.
+ * @n: size of the input array.
  *
  */
 void reverse_array(int *a, int n)
 {
 	int i = 0, temp;
 
-	n = n -1;
-	while (i <= ((n + 1) / 2))
+	n = n - 1;
+	while (i <= ((n + 1) / 2) && (n >= 0))
 	{
 		temp = a[i];
 		a[i] = a[n - i];
