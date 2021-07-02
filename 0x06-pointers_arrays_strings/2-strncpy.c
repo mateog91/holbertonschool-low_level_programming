df60d48d316ee082acdae8d1aa6e40876755ff22
Mateo <mateog91@gmail.com>
2021-07-02 21:22:13 +0000

    corrected

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,13 +8,18 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 
-	while (i <= n)
+	i = 0;
+	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
 
+	while (i < n; i++)
+	{
+		dest[i] = '\0'
+	}
 	return (dest);
 }
