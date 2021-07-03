d0d5949a76b11b6793b563499e1e7e8130c9c6ee
Mateo <mateog91@gmail.com>
2021-07-03 01:09:57 +0000

    added '.' in if for the seperators conditional

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,21 +10,23 @@
  */
 char *cap_string(char *s)
 {
-	int i, c;
+	int i;
+	char c;
 
+
+	/* Check if first char is lower case, if so change to upper */
+	i = 0;
+	if (s[i] >= 'a' && s[i] <= 'z')
+		s[i] = s[i] - 32;
 
 	/* Goes through string starting at position 1 */
-	for (i = 0; s[i]; i++)
+	for (i = 1; s[i]; i++)
 	{
-		/* Check if first char is lower case, if so change to upper */
-		if (s[i] >= 'a' && s[i] <= 'z')
-			s[i] = s[i] - 32;
-
 		/* Check if previous char is a seperator */
 		c = s[i - 1];
 		if (c == 9 || c == 10 || c == 32 || c == ',' || c == ';'
 			|| c == '!' || c == '?' || c == '(' || c == ')'
-			|| c == '{' || c == '}')
+			|| c == '{' || c == '}' || c == '.')
 		{
 			/* Change current char to upper case  */
 			if (s[i] >= 'a' && s[i] <= 'z')
