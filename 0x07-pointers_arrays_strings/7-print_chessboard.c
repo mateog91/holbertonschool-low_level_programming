77ddf39b5d25622bb77f083c32319950ffb870c1
Mateo <mateog91@gmail.com>
2021-07-06 20:30:41 +0000

    added file 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_chessboard - print a chessboard
+ * @a: array of chessboard
+ *
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			put_char(a[i][j];
+		}
+	}
+}
