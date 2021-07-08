933c73091c317753a549d02b4ba71532ae28d3be
Mateo <mateog91@gmail.com>
2021-07-08 22:11:54 +0000

    did it all over again

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,13 +1,35 @@
+int _palindrome2(char *s, int size);
+
 /**
  * is_palindrome - Function that checks if palindrome
  * @s: pointer to string
  *
  * Return: 1 if palindrome, 0 if not.
  */
+
 int is_palindrome(char *s)
 {
-	if (*s == '\0')
+	int size;
+
+	/* Find length of string */
+	for (size = 0; s[size] != '\0'; size++)
+	{
+	}
+	
+	return (_palindrome2(s, size));
+}
+
+int _palindrome2(char *s, int size)
+{
+	/* Base Cases */
+	if (*s != *(s + size - 1))
+		return (0);
+	else if (size == 2 && *s == *(s + size - 1))
+		return (1);
+	else if (size == 1)
 		return (1);
 	else
-		return (0);
+		/* Recursive Case */
+		return _palindrome2(s + 1, size - 2);
 }
+	
