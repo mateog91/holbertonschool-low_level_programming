d80a261d4220792007862f479118beb478b84902
Mateo <mateog91@gmail.com>
2021-07-08 04:53:08 +0000

    attempt 2

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,9 +1,13 @@
 /**
  * is_palindrome - Function that checks if palindrome
  * @s: pointer to string
+ *
  * Return: 1 if palindrome, 0 if not.
  */
 int is_palindrome(char *s)
 {
-	return (0);
+	if (s[0] == "")
+		return (1);
+	else
+		return (0);
 }
