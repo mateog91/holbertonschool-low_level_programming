b881ffa4461a00ac98c825f51a562044480b2628
Mateo <mateog91@gmail.com>
2021-07-08 02:27:06 +0000

    modified entire method

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
  * sqr_recursion2 - help function
  * @root: candidate to be a root of target
@@ -5,18 +7,30 @@
  *
  * Return: -1 if not a natural square root or natural square root of target
  */
-int sqr_recursion2(int root, int target)
+int sqr_recursion2(unsigned long root, unsigned long target)
 {
-	/* base case */
-	if (root < 0)
-		return (-1);
-	else if (root * root == target)
+	unsigned long square = root * root;
+
+	printf("root: %i\n", (int) root);
+
+	/* base case 0 y 1 */
+	if (square == target)
+	{
 		return (root);
-
-	/* recursive case */
-	return (sqr_recursion2(root - 1, target));
-}
-
+	}
+	else if (root == 1)
+		return (-1);
+	else if (square < target)
+	{
+		/* si al incrementar se pasa, significa que era menor y se volvio a pasar, por lo tanto no tiene solucion */
+		if (((root + 1) * (root + 1)) > target)
+			return (-1);
+		
+		return (sqr_recursion2(root + 1, target));
+	}
+	else
+		return (sqr_recursion2(root / 2, target));
+}	
 /**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: Interger input to find the square root of
@@ -25,5 +39,8 @@ int sqr_recursion2(int root, int target)
  */
 int _sqrt_recursion(int n)
 {
+	if (n < 0)
+		return (-1);
+
 	return (sqr_recursion2(n, n));
 }
