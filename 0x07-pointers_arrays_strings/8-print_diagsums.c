fd512504ac336e65d0d3a5052b665f7b4634924d
Mateo <mateog91@gmail.com>
2021-07-09 05:44:45 +0000

    add 8 corrected

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,34 +1,34 @@
 #include<stdio.h>
 
 /**
+ * get_element - gets element of the plane "matriz"
+ * @a: pointer to array
+ * @i: i index
+ * @j: j index
+ * @size: size of matrix
+ *
+ * Return: element inside matrix.
+ */
+int get_element(int *a, int i, int j, int size)
+{
+	return (*(a + i * size + j));
+}
+
+/**
  * print_diagsums - print sum of diagonals of a squared matrix
  * @a: Pointer to a 2D squared matrix of type interger.
  * @size: Size of the matrix.
  *
  */
-
 void print_diagsums(int *a, int size)
 {
-	int row, column, diagonal_sum1 = 0, diagonal_sum2 = 0;
+	int i, diagonal_sum1 = 0, diagonal_sum2 = 0;
 
-	/* Go throgh matix diagonal from top left to bottom right */
-	for (row = 0; row < size; row++)
+	for (i = 0; i < size; i++)
 	{
-		for (column = 0; column < size; column++)
-		{
-			diagonal_sum1 = diagonal_sum1 + a[row][column];
-		}
+		diagonal_sum1 += get_element(a, i, i, size);
+		diagonal_sum2 += get_element(a, i, size - 1 - i, size);
 	}
-	/* Go through matrix diagonal from bottom left to top right */
-
-	for (row = size - 1; row >= 0; row--)
-	{
-		for (column = 0; column < size; column++)
-		{
-			diagonal_sum2 = diagonal_sum2 + a[row][column];
-		}
-	}
-
 	/* Print diagonal sums */
-	pritf("%i, %i\n", diagonal_sum1, diagonal_sum2);
+	printf("%i, %i\n", diagonal_sum1, diagonal_sum2);
 }
