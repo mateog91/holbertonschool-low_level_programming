92e8a12317a40e636441e98d3e7a85ec25721843
Mateo <mateog91@gmail.com>
2021-07-14 11:45:02 +0000

    added free space before returning null

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -32,7 +32,10 @@ int **alloc_grid(int width, int height)
 	{
 		*(ptr + i) = (int *) malloc(sizeof(int) * height);
 		if (*(ptr + i) == NULL)
+		{
+			free(ptr);
 			return (NULL);
+		}
 	}
 	
 	for (i = 0; i < width; i++)
