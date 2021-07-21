70bc914b5da048b4c2bd03749fe8096c6d7085ba
Mateo <mateog91@gmail.com>
2021-07-21 19:32:41 +0000

    added shotr description

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,6 @@
 #include "function_pointers.h"
 /**
- * print_name + function that prints a name
+ * print_name - function that prints a name
  * @name: Input pointer to char with name
  * @f: Pointer to function
  *
