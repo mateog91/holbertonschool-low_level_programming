3296e5981cae7310982a9310fcdbb86d1361917e
Mateo <mateog91@gmail.com>
2021-07-23 23:58:48 +0000

    corrected for betty style

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,7 +3,7 @@
 
 /**
  * sum_them_all - Function that returns the sum of all its parameters
- * @n: n
+ * @n: count of variable parameters that are goint to be input to function
  *
  * Return:
  * sum of parameters, if n == 0, return 0
@@ -18,17 +18,17 @@ int sum_them_all(const unsigned int n, ...)
 
 	/* starts variadic macro */
 	va_start(list_number, n);
-	
+
 	/* Check if count of input numbers to sum is cero */
 	if (n == 0)
 		return (0);
 
 	sum = 0;
 	/* looks for the optional variables */
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 		sum = sum + va_arg(list_number, int);
 
-	va_end (list_number);
+	va_end(list_number);
 
 	return (sum);
 }
