473384112ee82bd5104430afaa8650b74dcaa898
Mateo <mateog91@gmail.com>
2021-07-23 23:56:50 +0000

    added file 0-sum_them_all.c a variadic function with first input as count of optional parameters and returns the sum of all the optional parameters inputed

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,34 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * sum_them_all - Function that returns the sum of all its parameters
+ * @n: n
+ *
+ * Return:
+ * sum of parameters, if n == 0, return 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	/* declares list_numbers as data stuture type va_list */
+	va_list list_number;
+	/* declares variables */
+	unsigned int i;
+	int sum;
+
+	/* starts variadic macro */
+	va_start(list_number, n);
+	
+	/* Check if count of input numbers to sum is cero */
+	if (n == 0)
+		return (0);
+
+	sum = 0;
+	/* looks for the optional variables */
+	for(i = 0; i < n; i++)
+		sum = sum + va_arg(list_number, int);
+
+	va_end (list_number);
+
+	return (sum);
+}
