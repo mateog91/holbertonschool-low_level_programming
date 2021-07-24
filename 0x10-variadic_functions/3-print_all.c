b5f0bb5ba88d18216b21ce18c1a3c7645b94922c
Mateo <mateog91@gmail.com>
2021-07-24 06:27:49 +0000

    added file 3-print_all that prints any kind of type elements for a variable number of inputs

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,96 @@
+#include "variadic_functions.h"
+
+void printchar(va_list list);
+void printint(va_list list);
+void printfloat(va_list list);
+void printstr(va_list list);
+/**
+ * print_all - function that prints anything
+ * @format: string with format of each passed variable to print
+ *
+ * Description:
+ * format is a list of types of arguments passed to the function
+ * c: char
+ * i: integer
+ * f: float
+ * s: char * (if the string is NULL, print (nil) instead
+ */
+void print_all(const char * const format, ...)
+{
+	td array_data[] = {
+		{"c", printchar},
+		{"i", printint},
+		{"f", printfloat},
+		{"s", printstr},
+		{NULL, NULL}
+	};
+	int i = 0, j;
+	void (*fun_ptr)(va_list);
+	char *separator = "";
+	va_list list;
+
+	va_start(list, format);
+	while (format[i] != '\0' && format != NULL)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == *(array_data[j].c))
+			{
+				printf("%s", separator);
+				fun_ptr = array_data[j].f;
+				fun_ptr(list);
+				separator = ", ";
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(list);
+	printf("\n");
+}
+
+/**
+ * printchar - prints a char
+ * @list: list with elements to print
+ */
+void printchar(va_list list)
+{
+	printf("%c", va_arg(list, int));
+}
+
+/**
+ * printint - prints a interger
+ * @list: list with elements to print
+ */
+void printint(va_list list)
+{
+	printf("%d", va_arg(list, int));
+}
+
+/**
+ * printfloat - prints a float
+ * @list: list with elements to print
+ */
+void printfloat(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+
+/**
+ * printstr - prints a string
+ * @list: list with elements to print
+ */
+void printstr(va_list list)
+{
+	char *string;
+
+	string = va_arg(list, char*);
+	if (string == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", string);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,6 +5,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct type_data - structure of the list that links the char with the funct
+ * @c: passedcharacter corresponding to a certain type
+ * @f: pointer to function corresponding to the character passed type
+ */
+typedef struct type_data
+{
+	char *c;
+	void (*f)();
+} td;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
