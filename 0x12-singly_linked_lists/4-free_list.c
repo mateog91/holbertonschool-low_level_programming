8cfed2560f04abc8a0986198eb12a4724b9be58f
Mateo <mateog91@gmail.com>
2021-07-31 04:58:51 +0000

    added

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,12 @@
+/**
+ * free_list - frees lists
+ * header: pointer to header
+ */
+void free_list(list_t *head);
+{
+	if (head == NULL)
+		return;
+	free_list(head->next);
+	free(head->str);
+	free(head);
+}
