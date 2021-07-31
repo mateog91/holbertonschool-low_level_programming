3829e0a6b55544be9757c99525705ac58171dd57
Mateo <mateog91@gmail.com>
2021-07-31 03:48:39 +0000

    added file 0 and corrected guard of list.h

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements of a list_t list.
+ * @h: Pointer to structure
+ *
+ * Return: Number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%i] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
+
