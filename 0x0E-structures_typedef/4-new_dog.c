2d3e05523b5517168b56b2a2e5d83c4f5e1a4b14
Mateo <mateog91@gmail.com>
2021-08-02 03:23:20 +0000

    corrected  check if allocation was ok for name and owner inside structure dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,7 +16,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
-	
+
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
