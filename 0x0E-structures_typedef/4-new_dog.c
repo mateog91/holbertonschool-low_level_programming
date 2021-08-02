d345c7579aa189cd64d1bd80bf297b36e9afe3d4
Mateo <mateog91@gmail.com>
2021-08-02 03:09:41 +0000

    changed sizeof to char and added copystring function for owner

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -44,7 +44,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog);
 		return (NULL);
 	}
-	new_dog->name = _strcpy(new_dog->name, name);
+	new_dog->owner = _strcpy(new_dog->owner, owner);
 	new_dog->age = age;
 	return (new_dog);
 }
