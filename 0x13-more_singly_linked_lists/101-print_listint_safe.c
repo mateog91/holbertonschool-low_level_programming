647db8150fe952b326c3fd88f1e101a213e15029
Mateo <mateog91@gmail.com>
2021-08-04 04:55:27 +0000

    added file 101

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,18 @@
+/**
+ * print_listint_safe - function that prints a listint_t linked list.
+ * @head: Pointer to first element
+ *
+ * Return: number of nodes in the loop
+ */
+size_t print_listint_safe(const listint_t *head);
+{
+
+	/* base cases */
+	if (h == NULL)
+		return (0);
+	/* Print current int */
+	printf("%i\n", h->n);
+
+	/* Recursive case */
+	return (1 + print_listint(h->next));
+}
