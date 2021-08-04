73ae00f6baa65737d81d570308c752e44634b992
Mateo <mateog91@gmail.com>
2021-08-04 00:58:55 +0000

    added check for double pointer to NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,6 +13,8 @@ void free_listint(listint_t *head);
  */
 void free_listint2(listint_t **head)
 {
+	if (head == NULL)
+		return;
 	/*use previous function to free the list */
 	free_listint(*head);
 
