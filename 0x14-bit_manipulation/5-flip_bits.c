3120d256e0872ba2bcf96c80e0e4cddbb307c6f4
Mateo <mateog91@gmail.com>
2021-08-06 05:21:50 +0000

    betty

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -7,20 +7,7 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	/*
-	int count = 0, i = (sizeof(n) * 8 - 1);
-	for (; i >= 0; i--)
-	{
-		if ((((m ^ n) >> i) & 1)  == 1)
-			count++;
-	}
-
-	return (count);
-	*/
-
-
 	if (n > 1)
-		return (((m ^ n) & 1) + flip_bits (n >> 1, m >> 1));
-
+		return (((m ^ n) & 1) + flip_bits(n >> 1, m >> 1));
 	return (m ^ n);
 }
