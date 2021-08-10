98060e7726022af8b877d068a3c40f5733ec1867
Mateo <mateog91@gmail.com>
2021-08-10 01:11:12 +0000

    changed return condition for empty file to 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 	{
 		close(fd);
-		return (-1);
+		return (1);
 	}
 	for (size = 0; text_content[size] != '\0'; size++)
 		;
