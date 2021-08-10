548e740cfc4a49e724ce70edc6e0cafa2d525614
Mateo <mateog91@gmail.com>
2021-08-10 13:05:12 +0000

    made corrections

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,34 +8,43 @@
  */
 int main(int argc, char *argv[])
 {
-	int lecture, writting, fileForm, fileTo, closeR;
-	char *buffer;
+	int lecture, writting, fileFrom, fileTo, closeFF, closeFT;
+	char *buffer = malloc(sizeof(char) * 1024);
 
-	if (arg != 3)
+	if (argc != 3)
 	{
 		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
 		exit(97); }
-	fileFrom = open(argv[1], O_RONLY );
-	if (fd == -1)
+	fileFrom = open(argv[1], O_RDONLY);
+	if (fileFrom == -1)
 	{
 		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98); }
-	fileTo = open(argv[2], O_WONLY | O_CREAT | O_TRUNC, 0664);
+	fileTo = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fileTo == -1)
 	{
-		dprintf("Error: Can't write to %s\n", argv[2]);
+		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99); }
-	while (lecture = read(fileFrom, buffer, 1024) > 0)
+	while ((lecture = read(fileFrom, buffer, 1024)) > 0)
 	{
-		if (lecture == -1)
-		{
-			dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98); }
 		writting = write(fileTo, buffer, lecture);
 		if (writting == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99); }
-	}
+			exit(99); } }
+	if (lecture == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98); }
+	closeFF = close(fileFrom);
+	if (closeFF == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileFrom);
+		exit(100); }
+	closeFT = close(fileTo);
+	if (closeFT == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileTo);
+		exit(100); }
 	return (0);
 }
