be6c819f76ab18e3b84045db88c5fd6137cc60eb
Mateo <mateog91@gmail.com>
2021-08-10 13:31:42 +0000

    corrected dprintf to standard error

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,17 +13,17 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97); }
 	fileFrom = open(argv[1], O_RDONLY);
 	if (fileFrom == -1)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98); }
 	fileTo = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fileTo == -1)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99); }
 	while ((lecture = read(fileFrom, buffer, 1024)) > 0)
 	{
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
 			exit(99); } }
 	if (lecture == -1)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98); }
 	closeFF = close(fileFrom);
 	if (closeFF == -1)
