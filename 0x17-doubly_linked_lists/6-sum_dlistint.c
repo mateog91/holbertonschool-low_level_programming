a05a6d55039de4a9d628c90dc485f6642cb2e697
Mateo <mateog91@gmail.com>
2021-09-17 10:50:31 -0500

    ADD: File 6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - function that returns the sum of all the data (n) of a
+ * listint_t linked list.
+ * @head: Pointer to first element of list
+ *
+ * Return:
+ * sum of all the data (n) of a listint_t linked list.
+ * if the list is empty, return 0
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	/* Base Case */
+	if (head == NULL)
+		return (0);
+	/* Recursive Case */
+	return ((head->n) + (sum_dlistint(head->next)));
+}
