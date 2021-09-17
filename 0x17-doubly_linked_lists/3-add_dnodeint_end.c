485b49fe02b986e7aacfd2be966a32120702c6fb
Mateo <mateog91@gmail.com>
2021-09-17 08:01:01 -0500

    STYLE: Trim whitespaces

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+/**
+ * add_dnodeint_end - Add node at end of list
+ * @head: head of list
+ * @n: number in list
+ *
+ * Return: Pointer to new node or NULL if failed
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *last;
+
+	if (head == NULL)
+		return (NULL);
+
+	printf("llego aqui");
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		dprintf(2, "Error: Can't malloc\n");
+		return (NULL);
+	}
+
+	/* new node creation and assign values */
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		*head = new;
+	}
+	else
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		new->prev = last;
+		last->next = new;
+	}
+	return (new);
+}
