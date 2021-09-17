6ed649889a686db3ac70c4d0a0ac2c7c46cc7231
Mateo <mateog91@gmail.com>
2021-09-17 08:16:35 -0500

    ADD: File 4 and 4-main

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - function that frees a listint_t list.
+ * @head: Pointer to head of list
+ *
+ * Return: Nothing.
+ */
+void free_dlistint(dlistint_t *head)
+{
+    /* Base Case: pointer to NULL means last node */
+    if (head == NULL)
+        return;
+    /* Recursive Case */
+    /* Free next node */
+    free_dlistint(head->next);
+    /* Reaches this point when reaches pointer to Null (last node) */
+    free(head);
+}
