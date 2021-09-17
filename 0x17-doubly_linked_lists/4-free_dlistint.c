7ca0c6790e2f2fdf0b70793ebc1e27557656e829
Mateo <mateog91@gmail.com>
2021-09-17 08:18:35 -0500

    STYLE: changed spaces to indent

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -8,12 +8,12 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-    /* Base Case: pointer to NULL means last node */
-    if (head == NULL)
-        return;
-    /* Recursive Case */
-    /* Free next node */
-    free_dlistint(head->next);
-    /* Reaches this point when reaches pointer to Null (last node) */
-    free(head);
+	/* Base Case: pointer to NULL means last node */
+	if (head == NULL)
+		return;
+	/* Recursive Case */
+	/* Free next node */
+	free_dlistint(head->next);
+	/* Reaches this point when reaches pointer to Null (last node) */
+	free(head);
 }
