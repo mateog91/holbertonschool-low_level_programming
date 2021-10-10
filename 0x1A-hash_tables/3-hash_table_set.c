5fb8673194f6b51c861ecf49e15c5276a3110ea4
Mateo <mateog91@gmail.com>
2021-10-10 18:54:37 -0500

    FIX: added advance current in search of key in bucketlist

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -33,6 +33,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 				return (0);
 			return (1);
 		}
+		current = current->next;
 	}
 	/* Bucket is empty or does not have key, add node*/
 	if (current == NULL)
