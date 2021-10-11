267c66df524f55aa37f8332b4ac9c62a03d50924
Mateo <mateog91@gmail.com>
2021-10-10 22:36:39 -0500

    FIX: ''

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -27,7 +27,7 @@ void hash_table_print(const hash_table_t *ht)
 			/* Print each element */
 			if (flag == 1)
 				printf(", ");
-			printf("%s : %s", current->key, current->value);
+			printf("'%s' : '%s'", current->key, current->value);
 			current = current->next;
 			flag = 1;
 		}
