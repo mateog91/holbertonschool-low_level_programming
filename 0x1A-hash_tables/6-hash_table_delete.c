337dc4bc536f2086a19acd0c1f2686e4a8176fc9
Mateo <mateog91@gmail.com>
2021-10-10 23:15:18 -0500

    STYLE

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,24 +9,24 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int i;
-    hash_node_t *current, *temp;
+	unsigned long int i;
+	hash_node_t *current, *temp;
 
-    if (ht == NULL)
-        return;
+	if (ht == NULL)
+		return;
 
-    for (i = 0; i < ht->size; i++)
-    {
-        current = ht->array[i];
-        while (current != NULL)
-        {
-            free(current->key);
-            free(current->value);
-            temp = current;
-            current = current->next;
-            free(temp);
-        }
-    }
-    free(ht->array);
-    free(ht);
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			free(current->key);
+			free(current->value);
+			temp = current;
+			current = current->next;
+			free(temp);
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
