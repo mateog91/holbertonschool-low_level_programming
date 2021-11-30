a3763a872b572994217cc74fa4c8d36f6c639ee4
Mateo <mateog91@gmail.com>
2021-11-30 14:31:21 -0500

    REDO

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -17,37 +17,65 @@ def island_perimeter(grid):
     i_max, j_max = 0, 0
     grid_width, grid_height = len(grid[0]), len(grid)
     i = 0
+    perimeter = 0
     while i < grid_height:
-        if 1 in grid[i]:
-            if i_min == -1 or i < j_min:
-                i_min = i
-            if i > i_max:
-                i_max = i
         j = 0
         while j < grid_width:
             current = grid[i][j]
             if current == 1:
-                if j_min == -1 or j < j_min:
-                    j_min = j
-                if j > j_max:
-                    j_max = j
+                # edge case column 0
+                if j == 0:
+                    perimeter += 1
+                elif grid[i][j-1] == 0:
+                    perimeter += 1
+
+                # edgecase last column
+                if j == grid_width - 1:
+                    perimeter += 1
+                elif grid[i][j+1] == 0:
+                    perimeter += 1
+
+                # edgecase row 0
+                if i == 0:
+                    perimeter += 1
+                elif grid[i-1][j] == 0:
+                    perimeter += 1
+
+                # edecase last row
+                if i == grid_height - 1:
+                    perimeter += 1
+                elif grid[i+1][j] == 0:
+                    perimeter += 1
             j += 1
         i += 1
 
-    if i_min == -1 and j_min == -1:
-        return 0
-
-    height = i_max + 1 - i_min
-    width = j_max + 1 - j_min
-    perimeter = (width + height) * 2
+    # height = i_max + 1 - i_min
+    # width = j_max + 1 - j_min
+    # perimeter = width * 2 + height * 2
     return perimeter
 
 
-grid = [
+grid1 = [
     [0, 0, 0, 0, 0, 0],
     [0, 1, 1, 0, 0, 0],
     [1, 1, 1, 0, 0, 0],
     [0, 1, 1, 1, 0, 0],
     [0, 0, 0, 1, 1, 1]
 ]
-print(island_perimeter(grid))
+
+grid2 = [
+    [0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+]
+
+grid3 = [
+    [0, 1, 0, 0, 0, 1],
+    [1, 1, 0, 0, 0, 1],
+    [1, 1, 0, 1, 1, 1],
+    [0, 1, 1, 1, 0, 0],
+    [0, 0, 1, 1, 0, 0]
+]
+print(island_perimeter(grid1))
+print(island_perimeter(grid2))
+print(island_perimeter(grid3))
